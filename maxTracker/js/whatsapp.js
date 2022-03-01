6505f3c5beecd6f77be8751a56e765dc2930518f
Armando <up190316@alumnos.upa.edu.mx>
2022-03-01 12:49:06 -0600

    wfeqws

--- maxTracker/js/whatsapp.js
+++ maxTracker/js/whatsapp.js
@@ -1,9 +1,11 @@
-function whatsApp(){
+function whatsApp() {
     var phone = "";
-    var seller =  Math.floor(Math.random()*1);
-    var text=encodeURIComponent('¡Quiero más información de Más Mensajes!');
-     switch(seller){
-       case 0: phone='5214491112375'; break;
-              }
-    window.open('https://api.whatsapp.com/send?phone='+phone+'&text='+text);
+    var seller = Math.floor(Math.random() * 1);
+    var text = encodeURIComponent('¡Quiero más información de Más Mensajes!');
+    switch (seller) {
+        case 0:
+            phone = '#';
+            break;
+    }
+    window.open('https://api.whatsapp.com/send?phone=' + phone + '&text=' + text);
 }

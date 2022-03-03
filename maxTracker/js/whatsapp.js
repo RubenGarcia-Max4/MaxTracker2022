fb31ce0afe9a99b5e9e6d4812180f738e8c6fc87
Armando <up190316@alumnos.upa.edu.mx>
2022-03-03 11:29:16 -0600

    changes in MaxFuel-BLOG

--- maxTracker/js/whatsapp.js
+++ maxTracker/js/whatsapp.js
@@ -1,10 +1,13 @@
 function whatsApp() {
     var phone = "";
-    var seller = Math.floor(Math.random() * 1);
+    var seller = Math.floor(Math.random() * 2);
     var text = encodeURIComponent('¡Quiero más información de Más Mensajes!');
     switch (seller) {
         case 0:
-            phone = '#';
+            phone = '4495166832';
+            break;
+        case 1:
+            phone = '4495504832';
             break;
     }
     window.open('https://api.whatsapp.com/send?phone=' + phone + '&text=' + text);
